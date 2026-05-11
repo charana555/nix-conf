4846390e8340007b06f0386a138de39a6e47c9fe
Charana555 <charanchandrashekar555@gmail.com>
2026-05-11 15:44:59 +0530

    added work packages

--- hosts/work/home.nix
+++ hosts/work/home.nix
@@ -7,4 +7,24 @@
   imports = [
     ../../home/common.nix
   ];
+
+  home.packages = with pkgs; [
+    # Rust toolchain (previously imperative)
+    cargo
+    rustc
+    
+    # Containers
+    docker-compose
+    
+    # Dev tools
+    gh
+    mkpasswd
+    
+    # Editors/utils
+    vim
+    xclip
+    
+    # TMUX (plugins managed manually via tpm)
+    tmux
+  ];
 }
