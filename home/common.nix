31cebb52c2ff7ec6c72b3c788034ecadf3bef2ef
Charana555 <charanchandrashekar555@gmail.com>
2026-06-05 16:19:07 +0530

    added experimental features

--- home/common.nix
+++ home/common.nix
@@ -5,6 +5,8 @@
 
   home.stateVersion = "25.05";
 
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   programs.home-manager.enable = true;
 
   xdg.configFile."opencode/opencode.json".source = ../opencode/opencode.json;
