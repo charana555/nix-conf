593c801ccf56b6da736166879115c848c6f57ee5
Charana555 <charanchandrashekar555@gmail.com>
2026-06-08 13:22:47 +0530

    fixed nixvim warning

--- home/modules/editor/nvim.nix
+++ home/modules/editor/nvim.nix
@@ -305,23 +305,14 @@
         settings = {
           ensure_installed = [ "bash" "c" "diff" "html" "lua" "luadoc" "markdown" "markdown_inline" "query" "vim" "vimdoc" "javascript" "typescript" "rust" "nix" ];
           auto_install = true;
-          highlight = {
-            enable = true;
-            additional_vim_regex_highlighting = [ "ruby" ];
-            disable.__raw = ''
-              function(lang, buf)
-                local max_filesize = 100 * 1024
-                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then
-                  return true
-                end
-              end
-            '';
-          };
-          indent = {
-            enable = true;
-            disable = [ "ruby" ];
-          };
+        };
+        highlight = {
+          enable = true;
+          additionalVimRegexHighlighting = [ "ruby" ];
+        };
+        indent = {
+          enable = true;
+          disable = [ "ruby" ];
         };
       };
 
--- home/modules/shell/zsh.nix
+++ home/modules/shell/zsh.nix
@@ -37,7 +37,7 @@ in
     shellAliases = {
       oc = "npx opencode-ai";
       to = "tmux a -t";
-      to-dsd = "ssh -o MACs=hmac-sha2-512-etm@openssh.com charana.c@dsd.persian-vega.ts.net -i ~/.ssh/id_ed25519_server";
+      to-dsd = "ssh -o MACs=hmac-sha2-512-etm@openssh.com charana.c@100.77.128.13  -i ~/.ssh/id_ed25519_server";
       to-cvps = "ssh -i ~/.ssh/id_ed25519 ubuntu@80.225.224.42";
       to-pvps = "ssh -i ~/.ssh/id_ed25519 ubuntu@140.245.225.52";
       to-server = "ssh -o MACs=hmac-sha2-512-etm@openssh.com charana.c@100.112.214.101 -i ~/.ssh/id_ed25519_server";
