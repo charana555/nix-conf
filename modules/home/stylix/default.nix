fa49901add3e2c3dd4acd4cd794d61a9142fecc5
Charana555 <charanchandrashekar555@gmail.com>
2026-07-23 18:45:41 +0530

    fix: disable stylix btop target on darwin (needs x86_64-linux builder)

--- modules/home/stylix/default.nix
+++ modules/home/stylix/default.nix
@@ -1,6 +1,7 @@
 {
   flake,
   lib,
+  pkgs,
   ...
 }:
 {
@@ -12,6 +13,9 @@
 
   # starship palette is derived manually in starship.nix
   config.stylix.targets.starship.enable = lib.mkForce false;
+
+  # btop theme derivation requires x86_64-linux builder
+  config.stylix.targets.btop.enable = !pkgs.stdenv.hostPlatform.isDarwin;
 
   # stylix hyprland target checks this attribute which doesn't exist
   # in current home-manager. Define it so the target doesn't error.
