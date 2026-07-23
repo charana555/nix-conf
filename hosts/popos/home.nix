8e6f00fd2fecefd9fbb95ad1f99a1afddae730f9
Charana555 <charanchandrashekar555@gmail.com>
2026-07-24 01:20:29 +0530

    fix: force-overwrite gtk.css on popos for stylix
    - GNOME desktop manages its own gtk.css, force=true lets stylix overwrite

--- hosts/popos/home.nix
+++ hosts/popos/home.nix
@@ -16,6 +16,11 @@ in
     flake.homeModules.stylix
   ];
 
+  home.file = {
+    ".config/gtk-3.0/gtk.css".force = true;
+    ".config/gtk-4.0/gtk.css".force = true;
+  };
+
   sops.secrets."private-keys/ssh" = {
     path = "${config.home.homeDirectory}/.ssh/id_ed25519";
     mode = "0600";
