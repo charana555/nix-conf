1d22ff907f61d60f591a28c5c43925a91b91a2ce
Charana555 <charanchandrashekar555@gmail.com>
2026-09-23 22:17:46 +0530

    Revert "feat(apps): add ytmdesktop on dell"
    This reverts commit b85c27e37b56f69c202312e181b8c0a3c9a1dd12.
    
    ytmdesktop opens but never loads YouTube Music on dell (blank
    "unable to load" screen), so it is not usable as shipped.
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- hosts/nixos/dell/default.nix
+++ hosts/nixos/dell/default.nix
@@ -147,7 +147,6 @@ in
   hm.apps.steam.enable = true;
   hm.apps.skLauncher.enable = true;
   hm.apps.localsend.enable = true;
-  hm.apps.ytmdesktop.enable = true;
 
   # LocalSend receives on 53317 (blocked by default firewall otherwise)
   networking.firewall.allowedTCPPorts = [ 53317 ];
