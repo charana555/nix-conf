44c9c83e500b2d88f5e51474822d847e95e55703
Charana555 <charanchandrashekar555@gmail.com>
2026-09-24 21:33:26 +0530

    fix(apps): only autoload .nix modules from apps dir
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- modules/home/apps/default.nix
+++ modules/home/apps/default.nix
@@ -1,7 +1,9 @@
-{ ... }:
+{ lib, ... }:
 
 {
   imports =
     with builtins;
-    map (file: ./${file}) (filter (file: file != "default.nix") (attrNames (readDir ./.)));
+    map (file: ./${file}) (
+      filter (file: file != "default.nix" && lib.hasSuffix ".nix" file) (attrNames (readDir ./.))
+    );
 }
